785d669af387b4312f90f5af73d3e2ed2b10e568
Aly Ibrahim <aly.ibrahim@gmail.com>
2021-03-30 05:14:34 -0500

    Add logging to node colors service for counter service check

--- node-colors-server/routes/index.js
+++ node-colors-server/routes/index.js
@@ -3,7 +3,13 @@ const { HttpError } = require('http-errors');
 var router = express.Router();
 const request = require('request');
 
-var counter_url = process.env.COUNTER_SERVICE +"/count/"+process.env.COLOR
+if (process.env.COUNTER_SERVICE == undefined){
+
+  console.log("COUNTER_SERVICE is not defined using localhost")
+  var counter_url = "http://127.0.0.1:8080/count/"+process.env.COLOR
+}else{
+  var counter_url = process.env.COUNTER_SERVICE +"/count/"+process.env.COLOR
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,13 +24,18 @@ router.post('/confirm', function(req, res, next) {
       if(err){
         console.log("Error counting " + res.statusCode)
         return
+      }else if (res.statusCode == 404){
+        console.log("Counter service is down");
+        return
       }
       console.log(body + "Counted");
     });
-    return res.json({'color': 'match'})
+    body.color = "match"
+    return res.json(body)
   }else{
     // res.status = 404
-    return res.send(404)
+    console.log("The service will only confirm " + process.env.COLOR + " but received " + body.color);
+    return res.sendStatus(404)
   }
 
 })
